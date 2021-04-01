320f03d2128f1dd6cbf99711e609b48a26d23ea3
E41191369_Oktafian Dwi Cahyono <71855139+oktafian510@users.noreply.github.com>
2021-04-01 12:48:45 +0700

    Minggu 4 / 1 dan 2
    template

---
+++ Minggu_4/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManagementUserController extends Controller
+{
+    public function index(){
+        // return "Halo ini adalah method index, dalam controller ManagementUser.";
+        $nama = "Oktafian Dwi Cahyono";
+        $pelajaran = ["Algoritma & Pemograman","Kalkulus","Matematika"];
+        return view('home', compact('nama','pelajaran'));
+    
+    }
+    public function create()
+    {
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk menambah data user";
+    }
+
+    public function store(Request $request)
+    {
+        return "Method ini nantinya akan digunakan untuk menciptakan data user yang baru";
+    }
+
+    public function show($id)
+    {
+        return "Method ini nantinya akan digunakan untuk menambil satu data user dengan id=" . $id;
+    }
+    public function edit($id)
+    {
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk mengubah data edit dengan id=" . $id;
+    }
+
+    public function update(Request $request, $id)
+    {
+        return "Method ini nantinya akan digunakan untuk mengubah data user dengan id=" . $id;
+    }
+
+    public function destroy($id)
+    {
+        return "Method ini nantinya digunakan untuk menghapus data user dengan id=" . $id;
+    }
+
+
+}
+
+?>
---
+++ Minggu_4/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get ('user','ManagementUserController@index');
+// Route::resource('user','ManagementUserController');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get("/home", function() {
+//     return view("home");
+// });
+Route::group(['namespace' => 'Frontend'], function() 
+{
+    Route::resource('home', 'HomeController');
+});
